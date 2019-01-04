6ceeca1368aaf33de7d1ad90984a0f6ad6be6fe2
shfshanyue <xianger94@gmail.com>
2019-01-04 16:28:00 +0800

    Add robots.txt

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const { createServer } = require('http')
 const next = require('next')
+const { parse } = require('url')
 
 const routes = require('./routes')
-
 
 const app = next({
   dev: process.env.NODE_ENV !== 'production'
 })
+
 const handler = routes.getRequestHandler(app)
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000)
+  createServer((req, res) => {
+    console.log(req.url)
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/robots.txt') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.write('User-agent: *\nSitemap: https://shici.xiange.tech/sitemap/site.xml')
+      res.end()
+    } else {
+      handler(req, res)
+    }
+  }).listen(3000)
 })
