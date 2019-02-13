cc072da448ab7e9e0da720fb5ba64017acbb7c1f
shfshanyue <xianger94@gmail.com>
2019-02-13 08:55:55 +0800

    Fix poem loading

--- components/Paragraph.js
+++ components/Paragraph.js
@@ -1,7 +1,7 @@
 import { map } from '../lib/utils'
 import Card from './Card'
 
-const Paragraph = ({ title, text, loading }) => text ? (
+const Paragraph = ({ title, text, loading }) => loading || text ? (
   <Card loading={loading}>
     <h3>{ title }</h3>
     { map(text, (t, index) => <p key={index}>{t}</p>) }
