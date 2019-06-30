a7c91bc575ce18a2902057983ec23f072ca4488f
shfshanyue <xianger94@gmail.com>
2019-06-30 19:49:36 +0800

    Update Card

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Skeleton from './Skeleton'
 
-function Card ({ children, loading }) {
+function Card ({ children, loading, title }) {
   return (
-    <div className={`card ${loading ? 'loading' : ''}`}>
-      <Skeleton active loading={loading}>
-        {children}
-      </Skeleton>
+    <div >
+      {
+        title && 
+          <div className="header">
+            { title }
+          </div>
+      }
+      <div className={`card ${loading ? 'loading' : ''}`}>
+        <Skeleton active loading={loading}>
+          {children}
+        </Skeleton>
+      </div>
       <style jsx>{`
       .card {
         padding: 20px; 
@@ -14,6 +22,15 @@ function Card ({ children, loading }) {
         margin-bottom: 15px;
         transition: all ease-out 0.2s;
         opacity: 1;
+      }
+
+      .header {
+        background-color: #fff;
+        height: 50px;
+        border-bottom: 1px solid #eee;
+        display: flex;
+        align-items: center;
+        padding: 0 20px;
       }
 
       @media (max-width: 575px) {
@@ -30,6 +47,5 @@ function Card ({ children, loading }) {
     </div>
   )
 }
-
 
 export default Card
--- components/Tags.js
+++ components/Tags.js
@@ -7,9 +7,9 @@ import Tag from './Tag'
 import Card from './Card'
 
 function Tags ({ tags }) {
-  return <Card>{
+  return <Card title="标签">{
     tags.slice(0, 50).map((tag) => (
-      <Tag style={{ margin: '5px' }}>
+      <Tag style={{ marginBottom: '10px' }}>
         <Link route="poems" params={{ tagId: tag.id, tagName: tag.name }} key={tag.id}>
           {tag.name}
         </Link>
