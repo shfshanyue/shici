1bc70805f67794b0836318e3a31e0a1e5f3b3fa0
shfshanyue <xianger94@gmail.com>
2019-07-10 08:48:40 +0800

    group tags

--- components/Tags.js
+++ components/Tags.js
@@ -1,28 +1,47 @@
 import React from 'react'
 import { graphql, compose } from 'react-apollo'
-import { Link } from "../routes";
+import { Link } from '../routes'
 import { TAGS } from '../query.gql'
+import { groupBy } from '../lib/utils'
 
 import Tag from './Tag'
 import Card from './Card'
 
 function Tags ({ tags = [] }) {
-  return <Card title="标签">{
-    tags.slice(0, 50).map((tag) => (
-      <Tag style={{ marginBottom: '10px' }}>
-        <Link route="poems" params={{ tagId: tag.id, tagName: tag.name }} key={tag.id}>
-          {tag.name}
-        </Link>
-      </Tag>
-    ))
-  }</Card>
+  return (
+    <Card title="标签">
+      <style jsx>{`
+        .tags:not(:last-child) {
+          border-bottom: 1px solid #eee;
+        }
+        .tags {
+          padding-top: 10px;
+        }
+      `}</style>
+      {
+        Object.entries(groupBy(tags, 'kind')).map(([kind, tags]) =>
+          <div key={kind} className="tags">
+            {
+              tags.map((tag) => (
+                <Tag style={{ marginBottom: '10px' }} key={tag.id}>
+                  <Link route="poems" params={{ tagId: tag.id, tagName: tag.name }} key={tag.id}>
+                    {tag.name}
+                  </Link>
+                </Tag>
+              ))
+            }
+          </div>
+        )
+      }
+    </Card>
+  )
 }
 
 export default compose(
   graphql(TAGS, {
     props ({ data }) {
       return {
-        tags: data.tags,
+        tags: data.tags.filter(tag => !new Set([1, 7, 8, 9, 10]).has(tag.kind)),
         loading: data.loading
       }
     }
