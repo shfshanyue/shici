5f49f839d382cae54c19a28aab2d29e816042345
shfshanyue <xianger94@gmail.com>
2019-08-21 22:30:57 +0800

    Refactor Paragraph

---
+++ components/Paragraph.tsx
@@ -0,0 +1,39 @@
+import Highlighter from 'react-highlight-words'
+import { map } from '../lib/utils'
+import Card from './Card'
+
+interface Props {
+  title: string;
+  text: string;
+  loading: boolean;
+  highlight: boolean;
+};
+
+function Paragraph ({ title, text, loading, highlight }: Props ) {
+  return loading || text ? (
+    <Card loading={loading}>
+      <style jsx>{`
+      p {
+        text-indent: 2em; 
+      } 
+    `}</style>
+      <h3>{title}</h3>
+      {
+        map(text, (t, index) => <p key={index}>
+          {
+            highlight ?
+              <Highlighter
+                highlightClassName="highlight"
+                highlightTag="span"
+                searchWords={[/「.+?」/, /“.+?”/, /《.+?》/]}
+                textToHighlight={t}
+              /> : t
+          }
+        </p>)
+      }
+    </Card>
+  ) : null
+}
+
+
+export default Paragraph
